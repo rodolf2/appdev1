44901f38a124bcdee00eb220536fbb98ee4fe8a8
rodolfo <rodolfojrxgt@gmail.com>
2024-10-13 23:41:24 +0800

    I’m done completing the section on adding styles.

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,26 +1,10 @@
 import './App.css'
 
-function MyButton() {
-  return (
-    <button>I'm a button</button>
-  )
-}
-
-function AboutPage() {
-  return (
-    <>
-      <h1>About</h1>
-      <p>Hello there.<br />How do you do?</p>
-    </>
-  )
-}
-
 function MyApp() {
   return (
     <>
       <h1>Welcome to my app</h1>
-      <MyButton />
-      <AboutPage />
+      <img className="avatar" src="https://i.imgur.com/yXOvdOSs.jpg" />
     </>
   )
 }
