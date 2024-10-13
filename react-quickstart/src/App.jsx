53106cce4929dd2612558adf6a2490428d5b2fe5
rodolfo <rodolfojrxgt@gmail.com>
2024-10-13 23:45:23 +0800

    I’m done completing the section on displaying data.

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
 
-function MyApp() {
+const user = {
+  name: 'Hedy Lamarr',
+  imageUrl: 'https://i.imgur.com/yXOvdOSs.jpg',
+  imageSize: 90,
+};
+
+function Profile() {
   return (
     <>
-      <h1>Welcome to my app</h1>
-      <img className="avatar" src="https://i.imgur.com/yXOvdOSs.jpg" />
+      <h1>{user.name}</h1>
+      <img
+        className="avatar"
+        src={user.imageUrl}
+        alt={'Photo of ' + user.name}
+        style={{
+          width: user.imageSize,
+          height: user.imageSize
+        }}
+      />
     </>
-  )
+  );
 }
 
-export default MyApp
+
+
+export default Profile
