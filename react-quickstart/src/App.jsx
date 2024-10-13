f5033366f9cda30be357250608c0b80b6303836c
rodolfo <rodolfojrxgt@gmail.com>
2024-10-13 23:26:05 +0800

    I’m done completing the section on writing markup with JSX.

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -6,11 +6,21 @@ function MyButton() {
   )
 }
 
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>Hello there.<br />How do you do?</p>
+    </>
+  )
+}
+
 function MyApp() {
   return (
     <>
       <h1>Welcome to my app</h1>
       <MyButton />
+      <AboutPage />
     </>
   )
 }
